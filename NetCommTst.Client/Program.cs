66e4abacd8fb767eb78192a97a46e6309f817850
Scott <daborg69@gmail.com>
2013-10-23 14:18:52 -0400

    Initial working version with 5K message size and 100K messages in 15 seconds.

---
+++ NetCommTst.Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DPSBase;
+using NetworkCommsDotNet;
+using System.Net;
+using System.Diagnostics;
+
+namespace ClientApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetworkComms.PacketConfirmationTimeoutMS = 20000;
+            ConnectionInfo serverConnectionInfo;
+
+            // Define destination address            
+            IPEndPoint IPE = IPTools.ParseEndPointFromString("10.1.53.44:19404");
+            serverConnectionInfo = new ConnectionInfo(IPE);
+
+            TCPConnection server = TCPConnection.GetConnection(serverConnectionInfo);
+
+            // Setup Send receive options
+            SendReceiveOptions nullCompressionSRO = new SendReceiveOptions(DPSManager.GetDataSerializer<ProtobufSerializer>(),
+                        new List<DataProcessor>(),
+                        new Dictionary<string, string>());            
+
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            StringBuilder sb = new StringBuilder("Start");
+
+            Console.WriteLine("Building string");
+            for (int i = 0; i < 500; i++)
+                sb.Append("abcdefghij");
+            string ms = sb.ToString();
+
+
+            Console.WriteLine("Testing Network sends");
+            int jj = 0;
+            for (jj=0;jj<100000;jj++)
+            {
+                //Send the message in a single line
+                server.SendObject("Fast", ms,nullCompressionSRO );
+            
+            }
+            sw.Stop();
+           
+            Console.WriteLine("Network test done");
+            Console.WriteLine("Time to send " + jj.ToString() + " messages of " + ms.Length + " was: " + sw.Elapsed.ToString() + " seconds");
+            Console.ReadLine();
+            //We have used comms so we make sure to call shutdown
+            NetworkComms.Shutdown();
+        }
+    }
+}
---
+++ NetCommTst.Server/Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetworkCommsDotNet;
+using System.Net;
+using System.Diagnostics;
+
+namespace ServerApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Trigger the method PrintIncomingMessage when a packet of type 'Message' is received
+            //We expect the incoming object to be a string which we state explicitly by using <string>
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Fast", PrintIncomingMessage);
+            //Start listening for incoming connections
+            //TCPConnection.StartListening(true);
+
+            NetworkComms.PacketConfirmationTimeoutMS = 20000;
+            //ConnectionInfo serverConnectionInfo;
+
+            // Define destination address            
+            IPEndPoint IPE = IPTools.ParseEndPointFromString("10.1.53.44:19404");
+            //serverConnectionInfo = new ConnectionInfo(IPE);
+
+            TCPConnection.StartListening(IPE);
+            //TCPConnection.GetConnection(serverConnectionInfo);
+            
+
+            //Print out the IPs and ports we are now listening on
+            Console.WriteLine("Server listening for TCP connection on:");
+            foreach (System.Net.IPEndPoint localEndPoint in TCPConnection.ExistingLocalListenEndPoints()) Console.WriteLine("{0}:{1}", localEndPoint.Address, localEndPoint.Port);
+
+            //Let the user close the server
+            Console.WriteLine("\nPress any key to close server.");
+            Console.ReadKey(true);
+
+            //We have used NetworkComms so we should ensure that we correctly call shutdown
+            NetworkComms.Shutdown();
+        }
+
+        /// <summary>
+        /// Writes the provided message to the console window
+        /// </summary>
+        /// <param name="header">The packetheader associated with the incoming message</param>
+        /// <param name="connection">The connection used by the incoming message</param>
+        /// <param name="message">The message to be printed to the console</param>
+        private static void PrintIncomingMessage(PacketHeader header, Connection connection, string message)
+        {
+           // Console.WriteLine("\nA message was recieved from " + connection.ToString() + " which said '" + message + "'.");
+        }
+    }
+}
