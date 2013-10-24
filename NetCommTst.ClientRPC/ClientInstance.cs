096936e65fd5777d6e1b59c6b07c64198660a5ef
daborg69 <daborg69@gmail.com>
2013-10-23 21:10:10 -0400

    Client RPC implemented
    8 Ball Working via RPC!

---
+++ NetCommTst.ClientRPC/ClientInstance.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProtoBuf;
+using NetCommTst.Common;
+using NetworkCommsDotNet;
+using System.Net;
+
+namespace NetCommTst.ClientRPC
+{
+    public class ClientInstance
+    {
+        private ConnectionType cxn;
+
+        public void Start()
+        {
+            Console.WriteLine("Establishing a TCP Connection...");
+
+            cxn = ConnectionType.TCP;
+            IPEndPoint IPE = IPTools.ParseEndPointFromString("127.0.0.1:19504");
+            ConnectionInfo CI = new ConnectionInfo(IPE);
+
+            Connection connection = TCPConnection.GetConnection(CI);
+            string instanceID = "";
+            IMagicEightBall remoteObj = SelectRemoteObject(connection, out instanceID);
+            Console.WriteLine("RPC instance has been created with instanceID: " + instanceID);
+
+            // Now play with the 8 ball.
+            while (true)
+            {
+                // Send RPC commands.
+                Console.WriteLine ("Resetting result was: " +  remoteObj.Reset());
+                Console.WriteLine ("Will I win the Lottery? Answer: " + remoteObj.AskQuestion ("Will I win the lottery?"));
+            }
+
+        }
+
+        private IMagicEightBall SelectRemoteObject(Connection connection, out string instanceID)
+        {
+            string instanceNamed ="MyEightBall";
+
+            // Private Client Object Instance
+            Console.WriteLine("Creating a Private Client Object Instance ");
+            return RemoteProcedureCalls.Client.CreateProxyToPrivateInstance<IMagicEightBall>(connection,instanceNamed,out instanceID);
+        }
+
+    }
+}
---
+++ NetCommTst.ClientRPC/ClientRPC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCommTst.ClientRPC
+{
+    class ClientRPC
+    {
+        static void Main(string[] args)
+        {
+            ClientInstance CI = new ClientInstance();
+            CI.Start();
+            Console.ReadLine();
+        }
+    }
+}
